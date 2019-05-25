3c3bc1df2e0240bc68e868c9eabedd863019767f
Samuel Xavier <sancrisxa@gmail.com>
2019-05-25 19:51:41 -0300

    Commit inicial

---
+++ pages/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Classificados</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="assets/js/jquery-3.3.2.min.js"></script>
+    <script src="assets/js/script.js"></script>
+</head>
+<body>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a href="./" class="navbar-brand">Classificados</a>
+            </div>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="">Cadastre-se</a></li>
+                <li><a href="">login</a></li>
+            </ul>
+        </div>
+    </nav> 
